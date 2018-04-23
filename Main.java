6728241dbb68f46829ed3419db9b4e6026e7b3b2
mahonard <whoispufferfish@gmail.com>
2018-04-22 20:55:22 -0400

    Added login idea in comments
    Also testing packages, added imports

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import BikePartDatabase.WareHouse;
+import BikePartDatabase.WareHouseDataBase;
+
 import java.io.PrintStream;
 import java.io.PrintWriter;
 
@@ -12,6 +16,23 @@ public class Main
 		WareHouseDataBase whDB = new WareHouseDataBase();
 		whDB.readWareHouseDB("warehouseDB.txt");
 		Commands cmds = new Commands(whDB);
+		
+		System.out.println("Enter username: ");
+		String userName = stan.nextLine();
+		System.out.println("Enter password: ");
+		String passWord = stan.nextLine();
+		String loginType = "";
+		
+		/**
+		 * for (user : userDataBase) 
+		 * {
+		 *    if (userName.equals(user.getUserName()) && passWord.equals(user.getPassWord())
+		 *    {
+		 *       loginType = user.getLoginType();
+		 *       return loginType;
+		 *    }
+		 * }
+		 */
 		
 		while (stopLoop)
 		{
